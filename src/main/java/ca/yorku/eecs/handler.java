4a3478bb63abcf3554b9834504c78d5727deecf3
KyongRok <kyongrokk@gmail.com>
2022-07-27 01:36:58 -0400

    working

--- src/main/java/ca/yorku/eecs/handler.java
+++ src/main/java/ca/yorku/eecs/handler.java
@@ -34,33 +34,58 @@ public class handler implements HttpHandler {
 	}
 	
 	public void GEThandler(HttpExchange request) throws IOException{
-		URI uri = request.getRequestURI();
+		String body = Utils.convert(request.getRequestBody());
+			try {
+				JSONObject jobj = new JSONObject(body);
+				URI uri = request.getRequestURI();
+				
+			} catch (JSONException e) {
+				
+				e.printStackTrace();
+			}
+			
+		
         
 	}
 	
 	public void PUThandler(HttpExchange request) throws IOException{
 		String body = Utils.convert(request.getRequestBody());
-		
-			
-			URI uri = request.getRequestURI();
-			String method = uri.getPath();
-			
-			if(method.equals("/api/v1/addActor")) {
-				System.out.println(body);
+			try {
+				JSONObject jobj = new JSONObject(body);
 				
-				sendString(request , "Yes" , 200);
-			}else if(method.equals("/api/v1/addMovie")) {
 				
-			}else if(method.equals("/api/v1/addRelation")) {
+				URI uri = request.getRequestURI();
+				String method = uri.getPath();
 				
-			}else {
-				String msg = "{msg : unimplemented method 501}";
-				sendString(request , msg , 501);
-				throw new IOException("unimplemented method 501\n" );
+				if(method.equals("/api/v1/addActor")) {
+					
+					//String actorId = jobj.getString("actorId");
+					//System.out.println(actorId);
+					String name = jobj.getString("name");
+					String actorId = jobj.getString("actorId");
+					
+					
+					sendString(request , "Yes" , 200);
+				}else if(method.equals("/api/v1/addMovie")) {
+					String name = jobj.getString("name");
+					String movieId = jobj.getString("movieId");
+					
+				}else if(method.equals("/api/v1/addRelation")) {
+					String actorId = jobj.getString("actorId");
+					String movieId = jobj.getString("movieId");
+					
+					
+				}else {
+					String msg = "{msg : unimplemented method 501}";
+					sendString(request , msg , 501);
+					throw new IOException("unimplemented method 501\n" );
+				}
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
 			}
+		
 			
-		
-		
 		 //"http://localhost8080/api/v1/addActor";
 		 //"http://localhost8080/api/v1/addMovie";
 		 //"http://localhost8080/api/v1/addRelation";
